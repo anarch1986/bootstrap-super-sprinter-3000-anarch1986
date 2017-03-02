7bd4c28aa055c8c8829c5ae9ecaf171c6a738da4
anarch1986 <varga.tamas19860808@gmail.com>
2017-03-02 11:07:47 +0100

    legacy files moved to this repo from the previous SI week project's repo

---
+++ models.py
@@ -0,0 +1,30 @@
+from peewee import *
+
+
+class CreateDatabase:
+
+    def create_db_object():
+        identify = open("connection.txt", "r")
+        login = identify.readlines()
+        identify.close()
+        db = PostgresqlDatabase(login[0], user=login[0])
+        return db
+
+
+class BaseModel(Model):
+
+    class Meta:
+        database = CreateDatabase.create_db_object()
+
+
+class UserStory(BaseModel):
+    title = CharField(max_length=100)
+    story = CharField(max_length=580)
+    criteria = CharField(max_length=580)
+    value = IntegerField()
+    estimation = FloatField()
+    status = CharField()
+
+    class Meta:
+        constraints = [Check("value BETWEEN 100 AND 1500"),
+                       Check("estimation BETWEEN 0.5 AND 40"), Check("status = 'Planning' OR status = 'To Do' OR status = 'In Progress' OR status = 'Review' OR status = 'Done'")]
---
+++ ssp3000.py
@@ -0,0 +1,79 @@
+from models import *
+from flask import Flask, request, g, redirect, url_for, \
+    render_template
+
+DEBUG = True
+
+app = Flask(__name__, static_url_path="/templates", static_folder="templates")
+app.config.from_object(__name__)
+
+
+def init_db():
+    db = CreateDatabase.create_db_object()
+    try:
+        db.connect()
+        db.create_tables([UserStory], safe=True)
+        print("Database connection established.")
+    except:
+        print("Can't connect to database.\nPlease check your connection.txt file.")
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'postgre_db'):
+        g.postgre_db.close()
+
+
+@app.route('/')
+@app.route('/list')
+def show_user_stories():
+    user_stories = UserStory.select().order_by(UserStory.id.asc())
+    return render_template('list.html', stories=user_stories)
+
+
+@app.route('/story')
+def empty_user_story():
+    return render_template('form.html', story=None)
+
+
+@app.route('/new_story', methods=['POST'])
+def add_user_story():
+    new_user_story = UserStory.create(title=request.form["story title"],
+                                      story=request.form["user story"], criteria=request.form[
+                                          "acceptance criteria"],
+                                      value=request.form[
+                                          "business value"], estimation=request.form["estimation"],
+                                      status=request.form["status"])
+    new_user_story.save()
+    return redirect(url_for('show_user_stories'))
+
+
+@app.route('/story/<story_id>', methods=['POST'])
+def get_user_story(story_id):
+    story_id = request.form["id_for_update"]
+    selected_story = UserStory.get(UserStory.id == story_id)
+    return render_template('form.html', story=selected_story)
+
+
+@app.route('/update', methods=['POST'])
+def update_user_story():
+    story_for_update = UserStory.update(title=request.form["story title"],
+                                        story=request.form["user story"], criteria=request.form[
+                                            "acceptance criteria"],
+                                        value=request.form[
+                                            "business value"], estimation=request.form["estimation"],
+                                        status=request.form["status"]).where(UserStory.id == request.form["id"])
+    story_for_update.execute()
+    return redirect(url_for('show_user_stories'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete_user_story():
+    story_id = request.form["id_for_delete"]
+    selected_story = UserStory.get(UserStory.id == story_id)
+    selected_story.delete_instance()
+    return redirect(url_for('show_user_stories'))
+
+if __name__ == "__main__":
+    init_db()
+    app.run(host='0.0.0.0')
